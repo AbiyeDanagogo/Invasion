9099d48eea38f5f94baba67e8f5d7961122718bf
Abiye <abiyedanagogo@gmail.com>
2020-09-02 10:57:14 +0100

    Changed class name to Alien

---
+++ app/src/main/java/com/abiyedanagogo/invasion/Alien.java
@@ -0,0 +1,62 @@
+package com.abiyedanagogo.invasion;
+
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Rect;
+
+import static com.abiyedanagogo.invasion.GameView.screenRatioX;
+import static com.abiyedanagogo.invasion.GameView.screenRatioY;
+
+public class Alien {
+
+    public int speed = 20;
+    public boolean wasShot = true;
+    int x = 0, y, width, height, alienCounter = 1;
+    Bitmap alien1, alien2, alien3, alien4;
+
+    Alien(Resources resources) {
+        alien1 = BitmapFactory.decodeResource(resources, R.drawable.alien1);
+        alien2 = BitmapFactory.decodeResource(resources, R.drawable.alien2);
+        alien3 = BitmapFactory.decodeResource(resources, R.drawable.alien3);
+        alien4 = BitmapFactory.decodeResource(resources, R.drawable.alien2);
+
+        width = alien1.getWidth();
+        height = alien1.getHeight();
+
+        width /=6;
+        height /=6;
+
+        width = (int) (width / screenRatioX);
+        height = (int) (height / screenRatioY);
+
+        alien1 = Bitmap.createScaledBitmap(alien1, width, height, false);
+        alien2 = Bitmap.createScaledBitmap(alien2, width, height, false);
+        alien3 = Bitmap.createScaledBitmap(alien3, width, height, false);
+        alien4 = Bitmap.createScaledBitmap(alien4, width, height, false);
+
+        y = -height;
+    }
+
+    Bitmap getAlien() {
+        if (alienCounter == 1) {
+            alienCounter++;
+            return alien1;
+        }
+        if (alienCounter == 2) {
+            alienCounter++;
+            return alien2;
+        }
+        if (alienCounter == 3) {
+            alienCounter++;
+            return alien3;
+        }
+
+        alienCounter = 1;
+        return alien4;
+    }
+//freee
+    Rect getCollisionShape() {
+        return new Rect((int) (x+(30/screenRatioX)), y, x+ width, y + height);
+    }
+}
